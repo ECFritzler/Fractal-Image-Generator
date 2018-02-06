9e92811292d762b119e84dd403db93d9199ae51b
E. Claire Fritzler <ec.fritzler@gmail.com>
2018-02-06 15:24:48 -0700

    updating hues

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@
 
 #include <iostream>
 #include "Bitmap.h"
+#include <iostream>
+#include <cstdint>
+#include <memory>
+#include <math.h>
+#include "Mandelbrot.hpp"
+#include "Bitmap.h"
 
 
 using namespace std;
@@ -20,7 +26,56 @@ int main() {
     
     Bitmap bitmap(WIDTH, HEIGHT);
     
-    bitmap.setPixel(WIDTH/2, HEIGHT/2, 255, 255, 255);
+    double min = 999999;
+    double max = -999999;
+    
+    unique_ptr<int[]> histo(new int[Mandelbrot::MAX_ITER] {});
+    unique_ptr<int[]> fractal(new int[WIDTH * HEIGHT] {});
+    
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            double xFrac = (x - WIDTH / 2 - 200) * 2.0 / HEIGHT;
+            double yFrac = (y - HEIGHT / 2) * 2.0 / HEIGHT;
+            
+            int iter = Mandelbrot::getIter(xFrac, yFrac);
+            
+            fractal[y * WIDTH + x] = iter;
+            
+            if (iter != Mandelbrot::MAX_ITER) {
+                histo[iter]++;
+            }
+            
+        }
+    }
+    
+    int total = 0;
+    for (int i = 0; i < Mandelbrot::MAX_ITER; i++)
+    {
+        total += histo[i];
+    }
+
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            
+            uint8_t red = 0;
+            uint8_t green = 0;
+            uint8_t blue = 0;
+            
+            int iter = fractal[y * WIDTH + x];
+            
+            if (iter != Mandelbrot::MAX_ITER) {
+                
+                double hue = 0.0;
+                
+                for (int i = 0; i <= iter; i++) {
+                    hue += ((double) histo[i]) / total;
+                }
+                
+                green = pow(255, hue);
+            }
+            bitmap.setPixel(x, y, red, green, blue);
+        }
+    }
     
     bitmap.writeBMP("test1.bmp");
     
