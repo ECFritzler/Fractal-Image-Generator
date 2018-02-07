63b3648808555d63c48663523e3cbbe3f9ef7c1c
ECFritzler <ec.fritzler@uleth.ca>
2018-02-07 10:40:49 -0700

    Finished zoom class

---
+++ FractalCreator.cpp
@@ -0,0 +1,9 @@
+//
+//  FractalCreator.cpp
+//  Fractal Image Generator
+//
+//  Created by Claire Fritzler on 2018-02-07.
+//  Copyright © 2018 Claire Fritzler. All rights reserved.
+//
+
+#include "FractalCreator.hpp"
---
+++ FractalCreator.hpp
@@ -0,0 +1,14 @@
+//
+//  FractalCreator.hpp
+//  Fractal Image Generator
+//
+//  Created by Claire Fritzler on 2018-02-07.
+//  Copyright © 2018 Claire Fritzler. All rights reserved.
+//
+
+#ifndef FractalCreator_hpp
+#define FractalCreator_hpp
+
+#include <stdio.h>
+
+#endif /* FractalCreator_hpp */
